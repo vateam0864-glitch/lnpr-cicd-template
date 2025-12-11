d434538d9d3fac98c201acce4539630acf94bf94
Dani <vateam0864@gmail.com>
2025-12-11 20:54:20 +0800

    working 1

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "LNPR App Running with correct package version!"
+    return "LNPR App Running with correct package version! hello "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
