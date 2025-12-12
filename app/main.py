cae5714313de0ec0f6ae06024b071420a41feb4e
Dani <vateam0864@gmail.com>
2025-12-12 13:02:47 +0800

    test restart fix

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "LNPR App Running with correct package version! Hello Dani! CI/CD working — updated version! "
+    return "LNPR App Running with correct package version! Hello Dani! CI/CD working — updated version! , added 2 line"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
