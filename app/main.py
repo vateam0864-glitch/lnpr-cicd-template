cf89ba2db9772f2593768178f61d65ce7db17206
Dani <vateam0864@gmail.com>
2025-12-12 13:29:50 +0800

    added date and time

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "LNPR App Running with correct package version! Hello Dani! CI/CD working — updated version! , added 2 line"
+    now = datetime.now()
+    current_dt = now.strftime("%Y-%m-%d %H:%M:%S")
+    return (
+        f"LNPR App Running with correct package version! "
+        f"Hello Dani! CI/CD working — updated version! , added 2 line "
+        f"(Current time: {current_dt})"
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
